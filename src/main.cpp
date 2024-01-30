37f8f7d0feeec3710106fa5507383d3e52a3b7b9
bryan-pignerol <90550463+bryan-pignerol@users.noreply.github.com>
2024-01-30 15:20:18 +0100

    Écriture première version du code

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,53 @@
 #include <Arduino.h>
+#include <SpaIot.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// NAMESPACE
+using namespace SpaIot;
+
+// GLOBAL VARIABLES
+ControlPanel spa ("SPAIOT32SSP"); // SPAIOT32SSP
+
+// FUNCTIONS
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  Serial.println("SPA INTEX IOT BY BRYAN PIGNEROL");
+
+  spa.begin();
+  while(spa.isOpen() == false) {
+    Serial.println("SPA NOT OPENED");
+    delay(2000);
+  }
+  Serial.println("SPA OPENED");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  spa.setPower(true); // Turn on the spa
+  Serial.println("SPA POWER ON");
+  delay(2000);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // BUBBLE
+  spa.setBubble(true);
+  Serial.println("SPA BUBBLE ON");
+  delay(5000);
+  spa.setBubble(false);
+  Serial.println("SPA BUBBLE OFF");
+
+  // FILTER
+  spa.setFilter(true);
+  Serial.println("SPA FILTER ON");
+  delay(5000);
+  spa.setFilter(false);
+  Serial.println("SPA FILTER OFF");
+
+  // HEATER
+  spa.setHeater(true);
+  Serial.println("SPA HEATER ON");
+  delay(5000);
+  spa.setHeater(false);
+  Serial.println("SPA HEATER OFF");
+
+  spa.setPower(false); // Turn off the spa
+  delay(10000);
 }
