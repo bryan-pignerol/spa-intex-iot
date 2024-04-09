1a3fa1465fe1afbf2d260d632f5c7e86bd622d66
bryan-pignerol <90550463+bryan-pignerol@users.noreply.github.com>
2024-04-09 19:35:26 +0200

    Création d'un nouveau test unitaire

---
+++ test/test_server/test_main.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+#include <SpaIot.h>
+#include <unity.h>
+#include <unity_config.h>
+
+using namespace SpaIot;
+
+// OBJECTS
+SpaServer serverSpa;
+
+// UNITY FUNCTIONS
+void setUp() {
+
+}
+
+void tearDown() {
+
+}
+
+void runUnityTests() {
+    UNITY_BEGIN()
+}
